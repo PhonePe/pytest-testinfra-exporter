52c235d9dd34056c307a7ed2f890e245267aec4b
Udeshya Giri <udeshyagiri@gmail.com>
2026-07-06 16:44:01 +0530

    SREAUTO-637 - Bump version to 0.3.0 and update maintainers list in pyproject.toml

--- pytest-testinfra-exporter/__init__.py
+++ pytest-testinfra-exporter/__init__.py
@@ -21,6 +21,6 @@ registration step.
 
 from .plugin import pytest_addoption, pytest_configure
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 __all__ = ["__version__", "pytest_addoption", "pytest_configure"]
